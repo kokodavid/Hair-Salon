a2e67cb84d7f0fd0042014602e8270a63f82e7a4
David mochoge <kingkgdave@gmail.com>
2019-04-22 19:00:22 +0300

    add stylist tests

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -1,8 +1,31 @@
+import java.util.List;
+import java.util.ArrayList;
+
+
 public class Stylist {
 
     private String mName;
+    private static List<Stylist> instances = new ArrayList<Stylist>();
+    private int mId;
 
     public Stylist(String name) {
         mName = name;
+        instances.add(this);
+        mId = instances.size();
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public static void clear() {
+        instances.clear();
+    }
+
+    public static List<Stylist> all() {
+        return instances;
+    }
+    public int getId() {
+        return mId;
     }
 }
--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -13,8 +13,31 @@ public class StylistTest {
     @Test
     public void Stylist_instantiatesWithName_String() {
         Stylist testStylist = new Stylist("Queen");
+        assertEquals("Queen", testStylist.getName() );
+    }
+
+    @Test
+    public void all_returnsAllInstancesOfStylist_true() {
+        Stylist firstStylist = new Stylist("Queen");
+        Stylist secondStylist = new Stylist("Ab");
+        assertEquals(true,Stylist.all().contains(firstStylist));
+        assertEquals(true,Stylist.all().contains(secondStylist));
 
     }
+
+    @Test
+    public void clear_emptiesAllStylistsFromList_0() {
+        Stylist testStylist = new Stylist("Queen");
+        Stylist.clear();
+        assertEquals(Stylist.all().size(),0);
+    }
+
+    @Test
+    public void getId_stylistInstantiatesWithAnId_1() {
+        Stylist testStylist = new Stylist("Queen");
+        assertEquals(1,testStylist.getId());
+    }
+
 
 
 }
